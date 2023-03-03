a4e76ee9842198a04812c39f6dc7eab339a32f65
eric.wagner <eric.wagner@etu.unistra.fr>
2023-03-03 10:05:29 +0100

    Serpent avec item collectable

--- js/main.js
+++ js/main.js
@@ -45,6 +45,33 @@ const anneauSize = 20;
 const gridWidth = Math.floor(canvas.width / anneauSize);
 const gridHeight = Math.floor(canvas.height / anneauSize);
 
+class CreateCollectibleItem {
+
+	constructor() {
+		this.i = getRandomInt(gridWidth);
+		this.j = getRandomInt(gridHeight);
+	}
+
+	// item = {
+	// 	x: getRandomInt(gridWidth) * anneauSize,
+	// 	y: getRandomInt(gridHeight) * anneauSize,
+	// 	size: anneauSize
+	// };
+
+	draw() {
+		ctx.fillStyle = '#ff00ff';
+		ctx.fillRect(this.i * anneauSize, this.j * anneauSize, anneauSize, anneauSize);
+	}
+
+	move() {
+		this.i = getRandomInt(gridWidth);
+		this.j = getRandomInt(gridHeight);
+	}
+
+
+
+}
+
 
 //---------- Classe Anneau ----------
 	
@@ -154,18 +181,28 @@ class Serpent {
 	}
 	
 	changeDirection(dir) {
-		this.dir = dir;
+		if (s.dir == 0 && dir == 2) {
+
+		} else if (s.dir == 1 && dir == 3) {
+
+		} else if (s.dir == 2 && dir == 0) {
+		
+		} else if (s.dir == 3 && dir == 1) {
+
+		} else {
+			this.dir = dir;
+		}
 	}
 
 	extends() {
 		const dernier_anneau = this.anneaux[this.longueur-1];
-		dernier_anneau.color = '#00ff00';
 		const avant_dernier_anneau = this.anneaux[this.longueur-2];
 		const nouvelle_position = {
 			x: dernier_anneau.i + (dernier_anneau.i - avant_dernier_anneau.i),
 			y: dernier_anneau.j + (dernier_anneau.j - avant_dernier_anneau.j)
 		};
-		const nouvel_anneau = new Anneau(nouvelle_position.x, nouvelle_position.y, '#0000ff');
+		const nouvel_anneau = new Anneau(nouvelle_position.x, nouvelle_position.y, dernier_anneau.color);
+		dernier_anneau.color = avant_dernier_anneau.color;
 		this.anneaux.push(nouvel_anneau);
 		this.longueur++;
 	}
@@ -173,6 +210,7 @@ class Serpent {
 }
 
 // Création d'un objet de la classe Serpent
+const item = new CreateCollectibleItem();
 const s = new Serpent(10, 10, 9, 1);
 
 //---------- Gestion de l'animation ----------
@@ -182,6 +220,14 @@ function anim() {
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
 	s.move();
 	s.draw();
+	item.draw();
+	
+	// console.log(item.i, item.j, s.anneaux[0].i, s.anneaux[0].j);
+	if (item.i == s.anneaux[0].i && item.j == s.anneaux[0].j) {
+		s.extends();
+		item.move();
+		item.draw();
+}	
 }
 
 // Identifiant du "timer"
@@ -228,6 +274,6 @@ document.addEventListener('keydown', function(event) {
     else if(event.key == "ArrowDown") {
         // alert('La flèche du bas a été préssée');
         s.changeDirection(2);
-        s.extends();
+        // s.extends();
     }
 });
